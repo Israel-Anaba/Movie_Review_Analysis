e5e4ae342b44a3f180928f6f86c59ddf0c5ea771
Israel Ayamga <officialanaba@gmail.com>
2023-11-06 03:01:14 +1100

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
+# builds a Docker image
 docker build -t movie-sentiment-prediction-app .
+
+# lists all Docker images
 docker images
+
+#  run a Docker container based on the image
 docker run -it imageid sh
+
+#  run Docker container
 docker run -p 7860:7860 --name movie-review-sentiment movie-sentiment-prediction-app
+
+#  list all containers 
 docker ps -a 
+
+# display the logs
 docker logs -f movie-review-sentiment       
